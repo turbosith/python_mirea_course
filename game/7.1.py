860dfe337b1f73da4ce8ebe347123560aaa0d82f
Evdokimov <danila.evdokimov.444@mail.ru>
2023-03-06 03:05:02 +0300

    code style

--- game/7.1.py
+++ game/7.1.py
@@ -1,10 +1,10 @@
 class Move:
-    def __init__(self, text, next_room_mark):
+    def __init__(self, text, next_location_mark):
         self.text = text
-        self.next_room_mark = next_room_mark
+        self.next_location_mark = next_location_mark
 
 
-class Room:
+class Location:
     def __init__(self, name=None, mark=None, description=None, next_movements=None):
         self.name = name
         self.mark = mark
@@ -14,98 +14,99 @@ class Room:
 
 class TextGame:
     def __init__(self):
-        self.rooms = {}
-        self.start_room = None
-        self.end_rooms = list()
+        self.locations = {}
+        self.start_location = None
+        self.end_locations = list()
 
-    def set_start_room(self, start_room):
-        self.rooms[start_room.mark] = start_room
-        self.start_room = start_room
+    def add_location(self, new_location):
+        self.locations[new_location.mark] = new_location
 
-    def add_end_room(self, end_room):
-        self.rooms[end_room.mark] = end_room
-        self.end_rooms.append(end_room.mark)
+    def set_start_location(self, start_location):
+        self.locations[start_location.mark] = start_location
+        self.start_location = start_location
 
-    def add_room(self, new_room):
-        self.rooms[new_room.mark] = new_room
+    def add_end_location(self, end_location):
+        self.locations[end_location.mark] = end_location
+        self.end_locations.append(end_location.mark)
 
     def start_game(self):
-        current_room = self.start_room
-        while current_room.mark not in self.end_rooms:
+        current_location = self.start_location
+        while current_location.mark not in self.end_locations:
             print()
-            print(current_room.name + "\n")
-            print(current_room.description)
-            for i in range(1, len(current_room.next_movements) + 1):
-                print(i, ') ' + current_room.next_movements.get(i).text)
-            room_decision = int(input())
+            print(current_location.name + "\n")
+            print(current_location.description)
+            for i in range(1, len(current_location.next_movements) + 1):
+                print(i, ') ' + current_location.next_movements.get(i).text)
+            location_decision = int(input())
 
-            current_room = self.rooms.get(current_room.next_movements.get(room_decision).next_room_mark)
+            current_location = self.locations.get(
+                current_location.next_movements.get(location_decision).next_location_mark)
 
-        print(current_room.description)
+        print(current_location.description)
 
 
-room1 = Room("Начало игры",
-             1,
-             "Вы в лесу, идёт дождь. Нужно найти, где укрыться от дождя",
-             {1: Move("Пойти на запад.", 2)})
+location1 = Location("Начало игры",
+                     1,
+                     "Вы в лесу, идёт дождь. Нужно найти, где укрыться от дождя",
+                     {1: Move("Пойти на запад.", 2)})
 
-room2 = Room("Локация №2",
-             2,
-             "Вы вышли на поляну. Деревья не урывают вас от дождя, нужно уйти отсюда, иначе вы промокните.",
-             {1: Move("Проход на запад.", 3),
-              2: Move("Проход на восток.", 1),
-              3: Move("Проход на юг.", 4)
-              })
+location2 = Location("Локация №2",
+                     2,
+                     "Вы вышли на поляну. Деревья не урывают вас от дождя, нужно уйти отсюда, иначе вы промокните.",
+                     {1: Move("Проход на запад.", 3),
+                      2: Move("Проход на восток.", 1),
+                      3: Move("Проход на юг.", 4)
+                      })
 
-room3 = Room("Локация №3",
-             3,
-             "Ааа, впереди яма. Придётся вернуться назад.",
-             {1: Move("Проход на восток", 2),
-              2: Move("Проход на запад", 5)
-              })
-room4 = Room("Локация №4",
-             4,
-             "Вы пришли к горе. Вдали веднеются огни деревни. Вы двигаетесь в правильном направлении!",
-             {1: Move("Проход на север", 2),
-              2: Move("Проход на юг", 6)
-              })
+location3 = Location("Локация №3",
+                     3,
+                     "Ааа, впереди яма. Придётся вернуться назад.",
+                     {1: Move("Проход на восток", 2),
+                      2: Move("Проход на запад", 5)
+                      })
+location4 = Location("Локация №4",
+                     4,
+                     "Вы пришли к горе. Вдали веднеются огни деревни. Вы двигаетесь в правильном направлении!",
+                     {1: Move("Проход на север", 2),
+                      2: Move("Проход на юг", 6)
+                      })
 
-room5 = Room(description="Вы упали в яму и умерли! Конец игры!", mark=5)
+location5 = Location(description="Вы упали в яму и умерли! Конец игры!", mark=5)
 
-room6 = Room("Локация №6",
-             6,
-             "Вы вышли к болоту. Придется его преодолеть.",
-             {1: Move("Проход на север", 4),
-              2: Move("Проход на восток", 7)
-              })
-room7 = Room("Локация №7",
-             7,
-             "Перед вами камень с надписью:" + "\n" +
-             "На север пойдешь: коня потеряешь." + "\n" +
-             "На запад пойдешь: болото найдешь. " + "\n" +
-             "На юг пойдешь: укрытие от дождя найдешь." + "\n" +
-             "На восток пойдешь: смерь найдешь.",
-             {1: Move("Проход на север", 8),
-              2: Move("Проход на юг", 9),
-              3: Move("Проход на восток", 10),
-              4: Move("Проход на запад", 6),
-              })
-room8 = Room("Локация №8",
-             8,
-             "У вас не было коня... Вы ничего не потеряли, но попали в тупик.",
-             {1: Move("Проход на юг", 7),
-              })
-room9 = Room(description="Вы нашли ночлег! Победа!", mark=9)
-room10 = Room(description="Вы нашли смерть! Поражение!", mark=10)
+location6 = Location("Локация №6",
+                     6,
+                     "Вы вышли к болоту. Придется его преодолеть.",
+                     {1: Move("Проход на север", 4),
+                      2: Move("Проход на восток", 7)
+                      })
+location7 = Location("Локация №7",
+                     7,
+                     "Перед вами камень с надписью:" + "\n" +
+                     "На север пойдешь: коня потеряешь." + "\n" +
+                     "На запад пойдешь: болото найдешь. " + "\n" +
+                     "На юг пойдешь: укрытие от дождя найдешь." + "\n" +
+                     "На восток пойдешь: смерь найдешь.",
+                     {1: Move("Проход на север", 8),
+                      2: Move("Проход на юг", 9),
+                      3: Move("Проход на восток", 10),
+                      4: Move("Проход на запад", 6),
+                      })
+location8 = Location("Локация №8",
+                     8,
+                     "У вас не было коня... Вы ничего не потеряли, но попали в тупик.",
+                     {1: Move("Проход на юг", 7),
+                      })
+location9 = Location(description="Вы нашли ночлег! Победа!", mark=9)
+location10 = Location(description="Вы нашли смерть! Поражение!", mark=10)
 game = TextGame()
-game.set_start_room(room1)
-game.add_room(room2)
-game.add_room(room3)
-game.add_room(room4)
-game.add_room(room6)
-game.add_room(room7)
-game.add_room(room8)
-game.add_end_room(room9)
-game.add_end_room(room10)
-game.add_end_room(room5)
+game.set_start_location(location1)
+game.add_location(location2)
+game.add_location(location3)
+game.add_location(location4)
+game.add_location(location6)
+game.add_location(location7)
+game.add_location(location8)
+game.add_end_location(location9)
+game.add_end_location(location10)
+game.add_end_location(location5)
 game.start_game()
